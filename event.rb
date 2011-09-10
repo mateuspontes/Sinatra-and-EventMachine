e1993b776faeefb6f70d6d3d5b4bef0329072043
Mateus <mateuspo10@gmail.com>
2011-09-10 19:40:56 -0300

    add time

--- event.rb
+++ event.rb
@@ -2,16 +2,19 @@ require 'rubygems'
 require 'em-websocket'
 
 EventMachine.run do
-  
+
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
-    ws.onopen { ws.send "Hello from EventMachine :)" }
+    ws.onopen { 
+      ws.send "Hello from EventMachine :)"
+    }
     
     ws.onclose { puts "Connection closed =/" }
     
     ws.onmessage { |msg| 
-      puts "Received: #{msg}"
-      ws.send "#{msg}"
+      t = Time.now()
+      ws.send "#{t.hour}h#{t.min}: #{msg}"
     }
   end
   
 end
+
