ffa5ee674b123304b86b9efb7af509ecb3ce3eab
Mateus <mateuspo10@gmail.com>
2011-09-10 21:22:03 -0300

    Add Broadcast o/

--- event.rb
+++ event.rb
@@ -2,18 +2,28 @@ require 'rubygems'
 require 'em-websocket'
 
 EventMachine.run do
+  
+  class ChatChannel < EventMachine::Channel
+  end
+  
+  @channel = ChatChannel.new
 
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
     ws.onopen { 
-      ws.send "Hello from EventMachine :)"
+      @sid = @channel.subscribe { |msg| ws.send msg }
+      ws.send("Welcome from EventMachine WebSocket :)")
+      
+      ws.onmessage { |msg| 
+        t = Time.now()
+        @channel.push "#{t.hour}h#{t.min} - Mateus: #{msg}"
+      }  
+      
+      ws.onclose { 
+        @channel.unsubscribe(@sid)
+        puts "Connection closed =/"
+      }
     }
     
-    ws.onclose { puts "Connection closed =/" }
-    
-    ws.onmessage { |msg| 
-      t = Time.now()
-      ws.send "#{t.hour}h#{t.min}: #{msg}"
-    }
   end
   
 end
